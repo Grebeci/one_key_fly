d4666e8ba87926aee4f123c6621e27e48b964bf3
XinXinWang <31677997+Grebeci@users.noreply.github.com>
2023-04-26 10:43:56 +0000

    one_key_fly

--- v2ray_server.sh
+++ v2ray_server.sh
@@ -31,7 +31,7 @@ function build_v2ray_server_for_debian() {
     
     apt-get update
     apt-get -y install cloudflare-warp
-    warp-cli delete
+    [[ "$(warp-cli status)" == *"Connected"*  ]] && warp-cli delete
     warp-cli register
     warp-cli set-mode proxy  # 必须先启动代理，如果参考官网上的跳过这个，本地ssh/ping就会连不到vps了
     warp-cli connect
